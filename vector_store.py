af0406080d14465ca73d6f06e008d0858df95aa3
ked-1823 <manekedar287@gmail.com>
2026-06-25 21:38:37 +0530

    added source citation2

--- vector_store.py
+++ vector_store.py
@@ -1,12 +1,27 @@
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
-import os
 from dotenv import load_dotenv
+import os
+import gc
 
 def create_vector_store(chunks):
     load_dotenv()
+
     persist_directory = "chroma_db"
-    embedding = OpenAIEmbeddings(model="text-embedding-3-small", openai_api_key=os.getenv("OPENAI_API_KEY"),
-                                 openai_api_base=os.getenv("OPENAI_BASE_URL"))
-    vector_store = Chroma.from_documents(chunks, embedding, persist_directory=persist_directory)
-    return vector_store
+
+    embedding = OpenAIEmbeddings(
+        model="text-embedding-3-small",
+        openai_api_key=os.getenv("OPENAI_API_KEY"),
+        openai_api_base=os.getenv("OPENAI_BASE_URL")
+    )
+
+    vector_store = Chroma.from_documents(
+        chunks,
+        embedding,
+        persist_directory=persist_directory
+    )
+
+    del vector_store
+    gc.collect()
+
+    return True
